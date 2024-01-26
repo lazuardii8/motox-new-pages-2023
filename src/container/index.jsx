7386bf203394fdbec6aeb99a5fbc9d6a9b8b4e9b
lazuardii8 <m.lazuardi.imani16@gmail.com>
2024-01-26 16:10:07 +0700

    fix bug QA

--- src/container/index.jsx
+++ src/container/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, Suspense } from "react";
+import React, { Fragment, Suspense, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // dashboard
@@ -6,6 +6,42 @@ const Homepage = React.lazy(() => import('./homepage/index'));
 const Error = React.lazy(() => import('./homepage/Error'));
 
 const index = (props) => {
+  useEffect(() => {
+    $('body').on('click touchstart', function () {
+      var videoSleep = document.querySelectorAll("video.lazy");
+      var videoSleepUp = document.querySelectorAll(".lazy video");
+
+      if (videoSleep.length >= 1) {
+        videoSleep.forEach(element => {
+          if (videoSleep.playing) {
+            // video is already playing so do nothing
+          } else {
+            element.play().then(() => {
+              // do something after successful playback
+            })
+              .catch(error => {
+                // handle any errors that occurred during playback
+              });
+          }
+        });
+      }
+      if (videoSleepUp.length >= 1) {
+        videoSleepUp.forEach(element => {
+          if (videoSleep.playing) {
+            // video is already playing so do nothing
+          } else {
+            element.play().then(() => {
+              // do something after successful playback
+            })
+              .catch(error => {
+                // handle any errors that occurred during playback
+              });
+          }
+        });
+      }
+    });
+  }, [])
+
   return (
     <Fragment>
       <Suspense fallback={<div></div>}>
